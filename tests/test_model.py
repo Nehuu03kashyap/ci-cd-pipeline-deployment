3a3ee7fc1c41bf38b4aad580ccdef7dfa278a451
nehuu03kashyap <neha03kash@gmail.com>
2024-09-05 13:15:58 +0530

    Initital Commit

---
+++ tests/test_model.py
@@ -0,0 +1,12 @@
+import unittest
+import joblib
+from sklearn.ensemble import RandomForestClassifier
+
+class TestModelTraining(unittest.TestCase):
+    def test_model_training(self):
+	model = joblib.load('model/iris_model.pkl')
+	self.assetIsInstance(model, RandomForestClasssifier)
+	swlf.assertGreaterEqual(len(model.feature_importances_), 4)
+
+if__name__ == '__main__':
+   unittest.main()
